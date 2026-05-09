b556d03c2cf2a9b7ba4a98a749b27b1c49d14f5c
Hugh MacNally <hugh.macnally@codewizards.co.uk>
2026-05-09 17:34:59 +0100

    Added TODO comments missed in previous commit.

--- services/handlers/verify.go
+++ services/handlers/verify.go
@@ -11,5 +11,8 @@ import "net/http"
 type VerifyHandler struct{}
 
 func (s *VerifyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// TODO: get puzzle out of request
+	// TODO: validate puzzle
+	// TODO: return result
 	w.Write([]byte("Placeholder"))
 }
